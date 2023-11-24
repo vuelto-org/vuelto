9c68c3413fb77c7947e0d099dc3de09060139a9d
Dima Marhitych <uzhdimka@gmail.com>
2023-11-24 16:41:46 +0100

    Delete cmake and some code formatting

--- src/Festi.h
+++ src/Festi.h
@@ -5,24 +5,23 @@ extern "C" {
 #include <GLFW/glfw3.h>
 
 typedef struct {
-	GLFWwindow* window;
-	int width;
-	int height;
-	const char* title;
+  GLFWwindow *window;
+  int width;
+  int height;
+  const char *title;
 } FS_Window;
 
 // Window.c
 void FS_Init();
-FS_Window* FS_CreateWindow(int width, int height, const char* title);
+FS_Window *FS_CreateWindow(int width, int height, const char *title);
 int FS_WindowShouldClose();
 void FS_Refresh();
 
 // Draw.c
-void FS_DrawRect(float x, float y, float height, float width, const float color[3]);
+void FS_DrawRect(float x, float y, float height, float width,
+                 const float color[3]);
 void FS_SetBackgroundColor(const float color[3]);
-
 
 #ifdef __cplusplus
 }
 #endif
-
--- src/festi/draw.c
+++ src/festi/draw.c
@@ -1,15 +1,16 @@
 #include "definitions.h"
 
-void FS_DrawRect(float x, float y, float height, float width, const float color[3]) {
-    glBegin(GL_QUADS);
-    glColor3f(color[0], color[1], color[2]);
-    glVertex2f(x, y);
-    glVertex2f(x + width, y);
-    glVertex2f(x + width, y + height);
-    glVertex2f(x, y + height);
-    glEnd();
+void FS_DrawRect(float x, float y, float height, float width,
+                 const float color[3]) {
+  glBegin(GL_QUADS);
+  glColor3f(color[0], color[1], color[2]);
+  glVertex2f(x, y);
+  glVertex2f(x + width, y);
+  glVertex2f(x + width, y + height);
+  glVertex2f(x, y + height);
+  glEnd();
 }
 
 void FS_SetBackgroundColor(const float color[3]) {
-	glClearColor(color[0], color[1], color[2], 1);
+  glClearColor(color[0], color[1], color[2], 1);
 }
