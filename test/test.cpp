abbf771df036ff88dfd6b01998d461063de0206c
Dima Marhitych <uzhdimka@gmail.com>
2023-11-28 13:21:56 +0100

    Software renderer uncommented

--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,13 @@
 #include "../src/Vuelto.hpp"
 
 int main() {
-  Vuelto::Application::Init();
+  Vuelto::Application::Init(true);
 
   Vuelto::Window win = Vuelto::Application::CreateWindow(800, 600, "test", true);
 
   while (!win.WindowShouldClose()) {
-    // Vuelto::SoftwareRenderer::SetBackgroundColor(255, 255, 255);
-    // Vuelto::SoftwareRenderer::DrawRect(550, 550, 200, 200, 155, 155, 155);
+    Vuelto::SoftwareRenderer::SetBackgroundColor(255, 255, 255);
+    Vuelto::SoftwareRenderer::DrawRect(550, 550, 200, 200, 155, 155, 155);
     Vuelto::Renderer2D::DrawRect(-0.1, -0.1, 0.5, 0.5, 0.1, 0.4, 0.7);
   }
 
