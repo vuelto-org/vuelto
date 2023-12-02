3aed528caa799b110f9fd32f766f48ecf76727eb
Dima Marhitych <uzhdimka@gmail.com>
2023-12-02 22:41:03 +0100

    Update definitions.hpp

--- include/vuelto/vuelto/tools/definitions.hpp
+++ include/vuelto/vuelto/tools/definitions.hpp
@@ -1,10 +1,15 @@
 #ifdef __APPLE__
+
 #define __gl_h_
 #define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
 #include <OpenGL/gl3.h>  // For macOS
+
 #elif defined _WIN32 || defined _WIN64
+
 #else
+
 #include <GL/glew.h>  // For other platforms
+
 #endif
 
 #include "../vendor/GLFW/glfw3.h"
