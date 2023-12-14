bd0b88ff85645fd15dbcb27c663a29d8c370b9ec
Dima Marhitych <uzhdimka@gmail.com>
2023-12-14 20:08:55 +0100

    Release version v1.0.0

--- test/test1/test.go
+++ test/test1/test.go
@@ -6,22 +6,30 @@ import (
 
 func main() {
   a := vuelto.NewApp()
-  w := a.NewWindow("hi", 800, 600, false)
+  w1 := a.NewWindow("hi", 800, 600, false)
+  w2 := a.NewWindow("hi2", 800, 600, false)
 
   ren := a.NewRenderer2D()
 
   image := ren.LoadImage("test/image.png", 300, 300, 250, 250)
   image1 := ren.LoadImage("test/image.png", 100, 100, 150, 150)
 
-  for !w.Close() {
+  for !w1.Close() && !w2.Close() {
+    w1.SetContextCurrent()
     ren.ClearColor([3]float32{0.3, 0.4, 0.3})
 
     ren.DrawRect(0, 0, 500, 500, [3]float32{0.1, 0.5, 0.7})
+    
+    w1.Refresh()
+    w2.SetContextCurrent()
+
 
     image.Draw()
     image1.Draw()
 
-    w.Refresh()
+    w2.Refresh()
+
   }
 }
+
 
