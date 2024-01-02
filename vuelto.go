9af57b6b1a56658cc5222f1f6700a5f51fda8e27
Dima Marhitych <uzhdimka@gmail.com>
2024-01-02 19:42:40 +0100

    Update vuelto.go

--- vuelto.go
+++ vuelto.go
@@ -9,7 +9,6 @@ type Window = src.Window
 type Image = src.Image
 type Renderer2D = src.Renderer2D
 type AudioPlayer = src.AudioPlayer
-type Animation = src.Animation
 
 func NewApp() src.Application {
 	return src.NewApp()
