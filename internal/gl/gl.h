aa827612db9e37384b68290461a4598138781276
Dima Marhitych <uzhdimka@gmail.com>
2024-02-13 15:40:26 +0100

    Update gl.h

--- internal/gl/gl.h
+++ internal/gl/gl.h
@@ -1,3 +1,6 @@
+#ifndef GL_H
+#define GL_H
+
 #ifdef __APPLE__
 #define GL_SILENCE_DEPRECATION
 #include <OpenGL/gl.h>
@@ -6,8 +9,14 @@
 #include <GL/gl.h>
 #elif _WIN32
 #define GL_CLAMP_TO_EDGE 0x812f
+#include <windows.h>
+#include <GL/gl.h>
 #elif _WIN64
 #define GL_CLAMP_TO_EDGE 0x812f
+#include <windows.h>
+#include <GL/gl.h>
 #else
 #include <GL/gl.h>
+#endif
+
 #endif
